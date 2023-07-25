034c815fff9ffe344337ded4c9595b0b9123484b
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-25 12:16:46 +0530

    Build Rest API

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask("Website")
+app = Flask(__name__)
 
-@app.route("/home")
+
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
 
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    df =pd.read_csv("")
+    temperature = df.station(date)
+    return {"station" : station,
+             "date": date,
+             "temperature": temperature}
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
